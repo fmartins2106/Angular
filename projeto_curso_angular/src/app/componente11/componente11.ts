12b1f43f39d4f66882dd2cc5fdf41f753b766a86
Fernando Martins <fernandom.adm@gmail.com>
2025-07-13 00:08:35 -0300

    Funcionalidades, validações para formulários em Angular

--- projeto_curso_angular/src/app/componente11/componente11.ts
+++ projeto_curso_angular/src/app/componente11/componente11.ts
@@ -1,12 +1,41 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Pessoa } from '../modelo/Pessoa';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-componente11',
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './componente11.html',
   styleUrl: './componente11.css'
 })
 export class Componente11 {
-  nome:string;
-  idade:number = null;
+
+  /* Objeto de formulário */
+  formulario = new FormGroup({
+    nome  : new FormControl('',[Validators.required, Validators.minLength(3)]),
+    idade : new FormControl(null, [Validators.required, Validators.min(0),Validators.max(120)]),
+    cidade: new FormControl('',[Validators.required, Validators.minLength(3)])
+  });
+
+   /* Visibilidade dos botões */
+    btnCadsatrar:boolean = true
+
+    /* lista */
+    lista: Pessoa[] = [];
+
+    /* Função de cadastro */
+
+    cadastrar(){
+      /* Cadastro vetor */
+      this.lista.push(this.formulario.value as Pessoa);
+
+      /* Limpeza dos inputs */
+      this.formulario.reset();
+
+      /* Visualização via console */
+     /*  console.table(this.lista); */
+
+    }
+
 }
