0cb02dd28a274414f82673b5bef2aa8c9e798618
Fernando Martins <fernandom.adm@gmail.com>
2025-07-16 16:52:00 -0300

    Aprendendo sobre Guard

--- projeto_curso_angular/src/app/app.routes.ts
+++ projeto_curso_angular/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { Pagina1 } from './paginas/pagina1/pagina1';
 import { Pagina2 } from './paginas/pagina2/pagina2';
 import { PaginaErro } from './paginas/pagina-erro/pagina-erro';
+import { rotaGuard } from './autenticacao/rota-guard';
+
 
 export const routes: Routes = [
   {path:'pagina1',component:Pagina1},
-  {path:'pagina2',component:Pagina2},
+  {path:'pagina2',component:Pagina2,canActivate:[rotaGuard]},
   {path:'',redirectTo:'/pagina1',pathMatch:'full'},
   {path:'',redirectTo:'/pagina2',pathMatch:'full'},
   {path:'**',component:PaginaErro}
---
+++ projeto_curso_angular/src/app/autenticacao/rota-guard.ts
@@ -0,0 +1,18 @@
+import { CanActivateFn, Router } from '@angular/router';
+
+export const rotaGuard: CanActivateFn = (route, state) => {
+
+  /* constante */
+  const rota = new Router;
+
+  /* Redirecionamento */
+
+  if(localStorage.getItem('nome') === 'Ralf'){
+    return true;
+  }else{
+    rota.navigateByUrl('pagina1');
+    return false;
+  }
+
+
+};
