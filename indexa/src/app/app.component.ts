a9ad661ab618f32f4663dd67400853cfcd1bc78f
Fernando Martins <fernandom.adm@gmail.com>
2025-10-01 01:48:54 -0300

    Adicionando novas funcionalidades na pagina principal

--- indexa/src/app/app.component.ts
+++ indexa/src/app/app.component.ts
@@ -3,16 +3,17 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { ContainerComponent } from './componentes/container/container.component';
 import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
+import { SeparadorComponent } from './componentes/separador/separador.component';
+import { ContatoComponent } from "./componentes/contato/contato.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet, ContainerComponent,
-    CabecalhoComponent
-  ],
+    CabecalhoComponent, SeparadorComponent, ContatoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'indexa';
+  alfabeto: string = 'abcdefghijklmnopqrstuvwxyz'
 }
